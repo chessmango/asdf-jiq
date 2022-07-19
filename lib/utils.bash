fdc2fb11f70cd7245a58e28facf636dd542be1b1
Owen Valentine <owenvalentine@gmail.com>
2022-07-19 13:16:22 +0200

    Third pass whee

--- lib/utils.bash
+++ lib/utils.bash
@@ -58,6 +58,7 @@ install_version() {
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 
     mkdir -p "$install_path"/bin
+    chmod +x "$ASDF_DOWNLOAD_PATH"/"$tool_cmd"
     cp "$ASDF_DOWNLOAD_PATH"/"$tool_cmd" "$install_path"/bin
 
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
